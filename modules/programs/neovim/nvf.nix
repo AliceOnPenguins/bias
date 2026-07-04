f262de1964047ed10dd9e625cfb892bb89b39765
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-04 14:47:12 +0200

    feat(neovim): integrate undofile for undo history

--- modules/programs/neovim/nvf.nix
+++ modules/programs/neovim/nvf.nix
@@ -1,61 +1,78 @@
 { self, inputs, ... }: {
-  flake.homeModules.neovim = { pkgs, lib, ... }: {
-    imports = [ inputs.nvf.homeManagerModules.default ];
+  flake.homeModules.neovim =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      imports = [ inputs.nvf.homeManagerModules.default ];
 
-    programs.nvf = {
-      enable = true;
-      settings.vim = {
-        viAlias = true;
-        vimAlias = true;
+      programs.nvf = {
+        enable = true;
+        settings.vim = {
+          viAlias = true;
+          vimAlias = true;
 
-        options = {
-          number = true;
-          relativenumber = true;
-          clipboard = "unnamedplus";
-          undofile = true;
-          undodir = "~/.local/state/nvim/undo/";
+          options = {
+            number = true;
+            relativenumber = true;
+            clipboard = "unnamedplus";
+            undofile = true;
+            undodir = "${config.home.homeDirectory}/.local/state/nvf/undo/";
+          };
+
+          statusline.lualine = {
+            enable = true;
+            theme = "auto";
+          };
+
+          utility.snacks-nvim = {
+            enable = true;
+            setupOpts = {
+              explorer = {
+              };
+              picker = {
+                sources = {
+                  explorer = {
+
+                  };
+                };
+              };
+            };
+          };
+
+          treesitter = {
+            enable = true;
+            fold = true;
+            context.enable = true;
+          };
+
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+          };
+          languages = {
+            nix.enable = true;
+            lua.enable = true;
+            bash.enable = true;
+            markdown.enable = true;
+          };
+
+          autocomplete.blink-cmp.enable = true;
+
+          git.gitsigns.enable = true;
+
+          autopairs.nvim-autopairs.enable = true;
+
+          binds.whichKey.enable = true;
+
+          formatter.conform-nvim.enable = true;
+
+          visuals.indent-blankline.enable = true;
+
         };
-
-        statusline.lualine = {
-          enable = true;
-          theme = "auto";
-        };
-
-        utility.snacks-nvim = {
-          enable = true;
-          setupOpts = { };
-        };
-
-        treesitter = {
-          enable = true;
-          fold = true;
-          context.enable = true;
-        };
-
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-        };
-        languages = {
-          nix.enable = true;
-          lua.enable = true;
-          bash.enable = true;
-          markdown.enable = true;
-        };
-
-        autocomplete.blink-cmp.enable = true;
-
-        git.gitsigns.enable = true;
-
-        autopairs.nvim-autopairs.enable = true;
-
-        binds.whichKey.enable = true;
-
-        formatter.conform-nvim.enable = true;
-
-        visuals.indent-blankline.enable = true;
-
       };
     };
-  };
 }
