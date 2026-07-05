e8357becc613af9a91f5c0910f39501b113f65c2
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-05 19:12:19 +0200

    style(theming): adjust hyprland animations to fit scrolling layout more

--- modules/programs/hyprland/hypr/animations.lua
+++ modules/programs/hyprland/hypr/animations.lua
@@ -8,14 +8,33 @@
 
 -- Self explanatory
 hl.config({
-	animations = {
-		enabled = true,
-	},
+  animations = {
+    enabled = true,
+  },
 })
 
-hl.curve("smooth", { type = "bezier", points = { {0.22, 1}, {0.36, 1} } })
+-- hl.curve("smooth", { type = "bezier", points = { { 0.22, 1 }, { 0.36, 1 } } })
+--
+-- hl.animation({ leaf = "workspaces", enabled = true, speed = 6, bezier = "smooth", style = "slidevert" })
+-- hl.animation({ leaf = "specialWorkspace", enabled = false, speed = 8, bezier = "smooth", style = "slidevert" })
+-- hl.animation({ leaf = "windows", enabled = true, speed = 3, bezier = "smooth" })
+-- hl.animation({ leaf = "fade", enabled = true, speed = 3, bezier = "smooth" })
 
-hl.animation({ leaf = "workspaces", enabled = true, speed = 6, bezier = "smooth", style = "slidevert" })
-hl.animation({ leaf = "specialWorkspace", enabled = false, speed = 8, bezier = "smooth", style = "slidevert" })
-hl.animation({ leaf = "windows",    enabled = true, speed = 3, bezier = "smooth" })
-hl.animation({ leaf = "fade",       enabled = true, speed = 3, bezier = "smooth" })
+-- new anims; credits: https://github.com/dusklinux/dusky
+hl.curve("pro", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.0 } } })
+hl.curve("snap", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.05 } } })
+
+hl.animation({ leaf = "windows", enabled = true, speed = 3, bezier = "snap", style = "popin 80%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, bezier = "snap", style = "popin 80%" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = 3, bezier = "snap" })
+
+hl.animation({ leaf = "layers", enabled = true, speed = 2, bezier = "pro", style = "slide" })
+hl.animation({ leaf = "fade", enabled = true, speed = 2, bezier = "pro" })
+hl.animation({ leaf = "border", enabled = true, speed = 3, bezier = "pro" })
+
+-- fix for screenshot gray capture
+hl.animation({ leaf = "layers", enabled = false })
+
+-- vert workspace slide
+hl.animation({ leaf = "workspaces", enabled = true, speed = 3, bezier = "pro", style = "slidevert" })
+hl.animation({ leaf = "specialWorkspace", enabled = true, speed = 3, bezier = "pro", style = "slidevert" })
