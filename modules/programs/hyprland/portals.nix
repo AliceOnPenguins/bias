a2cc35f70188c41c619246ba2daf7e1cf5a9c279
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-08 00:06:09 +0200

    fix(hyprland): fix eval error caused by wrong xdph version

--- modules/programs/hyprland/portals.nix
+++ modules/programs/hyprland/portals.nix
@@ -1,27 +1,29 @@
 { self, inputs, ... }: {
-    flake.homeModules.xdgPortals = { pkgs, lib, ... }: {
-        xdg.portal = {
-          enable = lib.mkForce true;
-          extraPortals = with pkgs; [
-            xdg-desktop-portal-wlr
-            xdg-desktop-portal-hyprland
-            xdg-desktop-portal-termfilechooser
-          ];
-        };
-        
-        xdg.portal.config.common = {
-          "default" = "hyprland;gtk";
-          "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
-        };
-        
-        home.sessionVariables.TERMCMD = "kitty --class=file_chooser";
-        
-        xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
-          force = true;
-          text = ''
-            [filechooser]
-            cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-          '';
-        };
+  flake.homeModules.xdgPortals =
+    { pkgs, lib, ... }:
+    {
+      xdg.portal = {
+        enable = lib.mkForce true;
+        extraPortals = [
+          pkgs.xdg-desktop-portal-wlr
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+          pkgs.xdg-desktop-portal-termfilechooser
+        ];
+      };
+
+      xdg.portal.config.common = {
+        "default" = "hyprland;gtk";
+        "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+      };
+
+      home.sessionVariables.TERMCMD = "kitty --class=file_chooser";
+
+      xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
+        force = true;
+        text = ''
+          [filechooser]
+          cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+        '';
+      };
     };
 }
