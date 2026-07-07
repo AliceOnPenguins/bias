1dc3d660eb69fb5ac8ee8ed8460fbf08c5152059
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-07 22:43:28 +0200

    feat(hyprland): move config to extraLuaFiles

--- modules/programs/hyprland/hyprland.nix
+++ modules/programs/hyprland/hyprland.nix
@@ -17,8 +17,62 @@
         package = null;
         portalPackage = null;
         systemd.enable = false;
+        plugins = [
+          pkgs.hyprlandPlugins.hy3
+        ];
+        extraLuaFiles = {
+          "animations" = {
+            content = ./hypr/animations.lua;
+            autoLoad = true;
+          };
+
+          "appearance" = {
+            content = ./hypr/appearance.lua;
+            autoLoad = true;
+          };
+
+          "env" = {
+            content = ./hypr/env.lua;
+            autoLoad = true;
+          };
+
+          "execs" = {
+            content = ./hypr/execs.lua;
+            autoLoad = true;
+          };
+
+          "general" = {
+            content = ./hypr/general.lua;
+            autoLoad = true;
+          };
+
+          "input" = {
+            content = ./hypr/input.lua;
+            autoLoad = true;
+          };
+
+          "keybinds" = {
+            content = ./hypr/keybinds.lua;
+            autoLoad = true;
+          };
+
+          "layout" = {
+            content = ./hypr/layout.lua;
+            autoLoad = true;
+          };
+
+          "monitor" = {
+            content = ./hypr/monitor.lua;
+            autoLoad = true;
+          };
+
+          "rules" = {
+            content = ./hypr/rules.lua;
+            autoLoad = true;
+          };
+
+        };
       };
-      xdg.configFile."hypr".source =
-        config.lib.file.mkOutOfStoreSymlink "/home/alice/bias/modules/programs/hyprland/hypr";
+      xdg.configFile."hypr/xdph.conf".source = ./hypr/xdph.conf;
     };
 }
