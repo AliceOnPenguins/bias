6a503781890b3f2b4043e15740562a37cc3878e4
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-10 17:11:02 +0200

    chore(cursor): set home.PointerCursor.enable as old method is deprecated

--- modules/icons-fonts-themes/Remus-White.nix
+++ modules/icons-fonts-themes/Remus-White.nix
@@ -1,25 +1,27 @@
 { self, inputs, ... }: {
-  flake.homeModules.remus-white = { pkgs, lib, ... }: 
-  let
-    remusWhite = pkgs.stdenv.mkDerivation {
-      pname = "remus-white-cursor";
-      version = "1.0";
-      src = ./assets/Remus-White;
-      dontBuild = true;
-      installPhase = ''
-        mkdir -p $out/share/icons/Remus-White
-        cp -r $src/* $out/share/icons/Remus-White/
-      '';
+  flake.homeModules.remus-white =
+    { pkgs, lib, ... }:
+    let
+      remusWhite = pkgs.stdenv.mkDerivation {
+        pname = "remus-white-cursor";
+        version = "1.0";
+        src = ./assets/Remus-White;
+        dontBuild = true;
+        installPhase = ''
+          mkdir -p $out/share/icons/Remus-White
+          cp -r $src/* $out/share/icons/Remus-White/
+        '';
+      };
+    in
+    {
+      home.packages = [ remusWhite ];
+      home.pointerCursor = {
+        enable = true;
+        gtk.enable = true;
+        x11.enable = true;
+        name = "Remus-White";
+        package = remusWhite;
+        size = 32;
+      };
     };
-  in
-  {
-    home.packages = [ remusWhite ];
-    home.pointerCursor = {
-      gtk.enable = true;
-      x11.enable = true;
-      name = "Remus-White";
-      package = remusWhite;
-      size = 32;
-    };
-  };
 }
