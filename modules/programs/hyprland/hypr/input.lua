cda7e82d2d62aaf5deaad4162ce69bd4f28352ae
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-18 23:45:55 +0200

    feat(hyprland): add cz-coder layout

--- modules/programs/hyprland/hypr/input.lua
+++ modules/programs/hyprland/hypr/input.lua
@@ -7,15 +7,16 @@
 ------------------------------------------
 
 hl.config({
-	input = {
-		kb_layout = "us",
-		kb_options = "grp:alt_shift_toggle",
-		repeat_delay = 275,
-		repeat_rate = 50,
-		numlock_by_default = true,
-		sensitivity = -0.7,
-		accel_profile = "flat",
-		follow_mouse = 1,
-		mouse_refocus = true,
-	},
+  input = {
+    kb_layout = "us,cz",
+    kb_variant = ",coder",
+    kb_options = "grp:alt_shift_toggle",
+    repeat_delay = 275,
+    repeat_rate = 50,
+    numlock_by_default = true,
+    sensitivity = -0.7,
+    accel_profile = "flat",
+    follow_mouse = 1,
+    mouse_refocus = true,
+  },
 })
