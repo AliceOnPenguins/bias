276531076116165e193b2d0874cfa4bd6f02b87c
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-22 11:34:08 +0200

    feat(shell): add zsh config

---
+++ modules/programs/shell/zsh.nix
@@ -0,0 +1,21 @@
+{ self, inputs, ... }: {
+  flake.homeModules.zsh = { pkgs, lib, ... }: {
+    programs.zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        vi = "nvim";
+        ls = "eza --icons -a";
+        nrs = "sudo nixos-rebuild switch --flake ~/bias#bias";
+        nru = "sudo nix flake update --flake ~/bias && sudo nixos-rebuild switch --flake ~/bias#bias";
+        nsp = "nix search nixpkgs";
+        ngc = "sudo nix-collect-garbage -d";
+        nem = "yazi ~/bias/";
+      };
+      initContent = ''
+        fastfetch
+      '';
+    };
+  };
+}
