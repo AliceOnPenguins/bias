39deddf6b0c81d1c7b5b9f610246d8ff6661dcd9
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-22 11:34:55 +0200

    feat(shell): disable fish

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -1,21 +1,21 @@
 { self, inputs, ... }: {
-    flake.homeModules.fish = { pkgs, lib, ... }: {
-      programs.fish = {
-        enable = true;
-        shellAliases = {
-          vi = "nvim";
-          ls = "eza --icons -a";
-          nrs = "sudo nixos-rebuild switch --flake ~/bias#bias";
-          nru = "sudo nix flake update --flake ~/bias && sudo nixos-rebuild switch --flake ~/bias#bias";
-          nsp = "nix search nixpkgs";
-          ngc = "sudo nix-collect-garbage -d";
-          nem = "yazi ~/bias/";
-        };
-        interactiveShellInit = ''
-          set fish_greeting
-          fastfetch
-        '';
+  flake.homeModules.fish = { pkgs, lib, ... }: {
+    programs.fish = {
+      enable = false;
+      shellAliases = {
+        vi = "nvim";
+        ls = "eza --icons -a";
+        nrs = "sudo nixos-rebuild switch --flake ~/bias#bias";
+        nru = "sudo nix flake update --flake ~/bias && sudo nixos-rebuild switch --flake ~/bias#bias";
+        nsp = "nix search nixpkgs";
+        ngc = "sudo nix-collect-garbage -d";
+        nem = "yazi ~/bias/";
       };
-
+      interactiveShellInit = ''
+        set fish_greeting
+        fastfetch
+      '';
     };
+
+  };
 }
