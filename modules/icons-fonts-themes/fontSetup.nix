9312f7906639f2bb0cf18ee4a256dcf21679cabb
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-23 15:20:17 +0200

    feat(font): add departure mono system font

--- modules/icons-fonts-themes/fontSetup.nix
+++ modules/icons-fonts-themes/fontSetup.nix
@@ -1,14 +1,15 @@
 { self, inputs, ... }: {
-    flake.nixosModules.fontSetup = { pkgs, lib, ... }: {
-        fonts = {
-            packages = with pkgs; [
-              nerd-fonts.jetbrains-mono
-              maple-mono.NF-CN-unhinted
-              ioskeley-mono.normal-NF
-              noto-fonts
-              noto-fonts-cjk-sans
-              noto-fonts-color-emoji
-            ];
-          };
-      };
-  }
+  flake.nixosModules.fontSetup = { pkgs, lib, ... }: {
+    fonts = {
+      packages = with pkgs; [
+        nerd-fonts.departure-mono
+        nerd-fonts.jetbrains-mono
+        maple-mono.NF-CN-unhinted
+        ioskeley-mono.normal-NF
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+      ];
+    };
+  };
+}
