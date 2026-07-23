42ea9d2ef179504f28f060f5d45dfd771bedcdf7
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-23 14:39:46 +0200

    fix(nix): scope rocmSupport to btop only, disable globally

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -1,35 +1,42 @@
 { self, inputs, ... }: {
-    flake.nixosModules.biasConfiguration = { pkgs, lib, ... }: {
-        imports = [
-        self.nixosModules.biasHardware
-        ];
+  flake.nixosModules.biasConfiguration = { pkgs, lib, ... }: {
+    imports = [
+      self.nixosModules.biasHardware
+    ];
 
-         nix.settings = {
-         experimental-features = [ "nix-command" "flakes" ];
-         trusted-users = [ "root" "alice" "@wheel" ];
-         # noctalia + hyprland
-        substituters = [
-           "https://hyprland.cachix.org"
-         ];
-         extra-substituters = [
-           "https://noctalia.cachix.org"
-           "https://hyprland.cachix.org"
-         ];
-         extra-trusted-public-keys = [ 
-           "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-         ];
-       };
-       
-         nixpkgs.config = {
-           rocmSupport = true;
-           permittedInsecurePackages = [
-              "electron-39.8.10"
-           ];
-           allowUnfree = true;
-         };
-       
-         # DO NOT CHANGE THIS VALUE MANUALLY
-         system.stateVersion = "26.11"; # Did you read the comment?
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "alice"
+        "@wheel"
+      ];
+      # noctalia + hyprland
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+      extra-substituters = [
+        "https://noctalia.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
+
+    nixpkgs.config = {
+      # rocmSupport = true;
+      permittedInsecurePackages = [
+        "electron-39.8.10"
+      ];
+      allowUnfree = true;
+    };
+
+    # DO NOT CHANGE THIS VALUE MANUALLY
+    system.stateVersion = "26.11"; # Did you read the comment?
+  };
 }
