2b58afe51b8cf36f62204b16f34c9ec025b70a83
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-07-24 17:56:08 +0200

    feat(font): rm unused fonts

--- modules/icons-fonts-themes/fontSetup.nix
+++ modules/icons-fonts-themes/fontSetup.nix
@@ -2,10 +2,9 @@
   flake.nixosModules.fontSetup = { pkgs, lib, ... }: {
     fonts = {
       packages = with pkgs; [
-        nerd-fonts.departure-mono
+        plemoljp-nf
         nerd-fonts.jetbrains-mono
         maple-mono.NF-CN-unhinted
-        ioskeley-mono.normal-NF
         noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-color-emoji
