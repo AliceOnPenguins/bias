73564f452db05ff3fa23574d5e969c8b22e5976c
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-08-08 01:23:46 +0200

    fix(user): add user to ydotool group

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -1,11 +1,20 @@
 { self, inputs, ... }: {
-    flake.nixosModules.usersAlice = { pkgs, lib, ... }: {
-        users.users.alice = {
-          uid = 1000;
-          isNormalUser = true;
-          extraGroups = [ "wheel" "mlocate" "video" "audio" "input" "networkmanager" "docker" ];
-          home = "/home/alice";
-        };
-
+  flake.nixosModules.usersAlice = { pkgs, lib, ... }: {
+    users.users.alice = {
+      uid = 1000;
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "mlocate"
+        "video"
+        "audio"
+        "input"
+        "networkmanager"
+        "docker"
+        "ydotool"
+      ];
+      home = "/home/alice";
     };
-  }
+
+  };
+}
