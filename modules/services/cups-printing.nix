ab2a4fed5063e888b1307d13e6f2744e1fba5cfb
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-09-16 12:11:36 +0200

    fix(services): fix hp printers not being recognized

--- modules/services/cups-printing.nix
+++ modules/services/cups-printing.nix
@@ -1,7 +1,15 @@
 { self, inputs, ... }: {
-    flake.nixosModules.cups-printing = { pkgs, lib, ... }: {
-        services.printing = {
-            enable = true;
-          };
-      };
-  }
+  flake.nixosModules.cups-printing = { pkgs, lib, ... }: {
+    services.printing = {
+      enable = true;
+      browsing = true;
+      drivers = [ pkgs.hplip ];
+    };
+
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+  };
+}
