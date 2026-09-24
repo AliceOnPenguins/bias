94736c16aa1a31e330cf22ecb2e8bf8028613375
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-09-24 21:38:00 +0200

    feat(discord): add new theme

--- modules/programs/discord/discordtheme.nix
+++ modules/programs/discord/discordtheme.nix
@@ -1,7 +1,10 @@
 { self, inputs, ... }: {
-    flake.homeModules.discordTheme = { pkgs, lib, ... }: {
-        xdg.configFile."equibop/themes/yunahDark.css" = {
-            source = ./yunahDark.css;
-          };
-      };
-  }
+  flake.homeModules.discordTheme = { pkgs, lib, ... }: {
+    xdg.configFile."equibop/themes/yunahDark.css" = {
+      source = ./yunahDark.css;
+    };
+    xdg.configFile."equibop/themes/lsDark.css" = {
+      source = ./lsDark.css;
+    };
+  };
+}
