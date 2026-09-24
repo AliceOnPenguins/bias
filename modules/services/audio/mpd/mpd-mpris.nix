c6b16137bbba7a8e4de957cd5395a2931ab48e73
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-09-24 22:07:41 +0200

    chore(mpd-mpris): fix eval error

--- modules/services/audio/mpd/mpd-mpris.nix
+++ modules/services/audio/mpd/mpd-mpris.nix
@@ -1,9 +1,9 @@
 { self, inputs, ... }: {
-    flake.homeModules.mpd-mpris = { pkgs, lib, ... }: {
-        services.mpd-mpris = {
-            enable = true;
-            mpd.host = "/run/user/1000/mpd/socket";
-            mpd.useLocal = true;
-          };
-      };
-  }
+  flake.homeModules.mpd-mpris = { pkgs, lib, ... }: {
+    services.mpd-mpris = {
+      enable = true;
+      settings.host = "/run/user/1000/mpd/socket";
+      # mpd.useLocal = true;
+    };
+  };
+}
