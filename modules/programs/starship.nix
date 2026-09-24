7363d021548229c8b02c88df501306a978dab3e5
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-09-24 21:45:17 +0200

    feat(starship): change color palette

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -1,44 +1,44 @@
 { self, inputs, ... }: {
-    flake.homeModules.starship = { pkgs, lib, ... }: {
-      programs.starship = {
-          enable = true;
-          enableFishIntegration = true;
-          settings = {
-            format = "[╭](dimmed white)[─](dimmed white)[ $username](bold #b8d4e8)[ in](dimmed white) [$directory](white)$git_branch$git_status\n[╰─❯](dimmed white) ";
-            username = {
-                format = "[$user]($style)";
-                show_always = true;
-                style_user = "bold #b8d4e8";
-            };
-            directory = {
-                format = "[ ](#b8d4e8)[$path](#b8d4e8)";
-                truncation_length = 3;
-                truncate_to_repo = true;
-                style = "";
-            };
-            git_branch = {
-                format = " [[ ](#f5b8cf bold)$branch](#f5b8cf)";
-                symbol = "";
-            };
-            git_status = {
-                format = "[ $all_status$ahead_behind](#b8d4e8)";
-                conflicted = "=";
-                ahead =	"⇡\${count}";
-                behind = "⇣\${count}";
-                diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
-                up_to_date = " 󰄬 ";
-                untracked = " ?\${count} ";
-                stashed = " 󰏖 ";
-                modified = " !\${count} ";
-                staged = " +\${count} ";
-                renamed = " »\${count} ";
-                deleted = " 󰅖\${count} ";
-            };
-            character = {
-                success_symbol = "";
-                error_symbol = "";
-            };
-          };
+  flake.homeModules.starship = { pkgs, lib, ... }: {
+    programs.starship = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        format = "[╭](dimmed white)[─](dimmed white)[ $username](bold #ff9a56)[ in](dimmed white) [$directory](white)$git_branch$git_status\n[╰─❯](dimmed white) ";
+        username = {
+          format = "[$user]($style)";
+          show_always = true;
+          style_user = "bold #ff9a56";
+        };
+        directory = {
+          format = "[ ](#ff9a56)[$path](#ff9a56)";
+          truncation_length = 3;
+          truncate_to_repo = true;
+          style = "";
+        };
+        git_branch = {
+          format = " [[ ](#e0609a bold)$branch](#e0609a)";
+          symbol = "";
+        };
+        git_status = {
+          format = "[ $all_status$ahead_behind](#ff9a56)";
+          conflicted = "=";
+          ahead = "⇡\${count}";
+          behind = "⇣\${count}";
+          diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+          up_to_date = " 󰄬 ";
+          untracked = " ?\${count} ";
+          stashed = " 󰏖 ";
+          modified = " !\${count} ";
+          staged = " +\${count} ";
+          renamed = " »\${count} ";
+          deleted = " 󰅖\${count} ";
+        };
+        character = {
+          success_symbol = "";
+          error_symbol = "";
+        };
       };
     };
+  };
 }
